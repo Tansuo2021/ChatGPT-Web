cc9975ab4179c37658be27aa7360e181ab16fcd9
Tansuo2021 <95366753+Tansuo2021@users.noreply.github.com>
2023-04-26 21:43:22 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,8 +16,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      name: '探索分享',
+      description: '<a href="https://shop.iosshop.xyz" class="text-blue-500" target="_blank" >购买ChatGPT</a> <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
